e3e8f6339d560d4d10fd831e575ed6476b3dee1d
D4rky23 <andrei_alexandru2305@yahoo.com>
2024-03-07 22:14:18 +0200

    QuizzApp 1.0

---
+++ app/src/main/java/com/example/quizzapp/QuestionAnswer.java
@@ -0,0 +1,57 @@
+package com.example.quizzapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "What is the chemical symbol for iron?",
+            "Who wrote the novel 'To Kill a Mockingbird'?",
+    "What is the largest ocean on Earth?",
+    "What is the capital city of France?",
+    "Who discovered penicillin?",
+    "What is the boiling point of water in Celsius?",
+    "Who painted the famous artwork 'The Persistence of Memory'?",
+    "What is the chemical formula for table salt (sodium chloride)?",
+    "Which planet is known as the 'Morning Star'?",
+    "Who was the first female Prime Minister of the United Kingdom?",
+    "What is the process by which plants convert sunlight into energy?",
+    "Who wrote the play 'Hamlet'?",
+    "What is the currency of Japan?",
+    "What is the chemical symbol for hydrogen?",
+    "Who was the first person to step onto the moon?"
+    };
+
+    public static String[][] choices ={
+            {"A) Fe", "B) Ir", "C) In", "D) I"},
+            {"A) Ernest Hemingway"," B) Harper Lee", "C) J.K. Rowling"," D) Mark Twain"},
+        {"A) Atlantic Ocean"," B) Indian Ocean", "C) Arctic Ocean", "D) Pacific Ocean"},
+            {"A) Rome", "B) Madrid", "C) Paris", "D) Berlin"},
+        {"A) Alexander Fleming", "B) Louis Pasteur", "C) Marie Curie", "D) Albert Einstein"},
+        {"A) 100°C", "B) 0°C"," C) 50°C"," D) 212°C"},
+        {"A) Pablo Picasso"," B) Salvador Dalí"," C) Vincent van Gogh", "D) Leonardo da Vinci"},
+        {"A) NaCl2", "B) NaOH"," C) H2SO4", "D) NaCl"},
+        {"A) Venus", "B) Mars", "C) Jupiter"," D) Mercury"},
+        {"A) Margaret Thatcher"," B) Theresa May"," C) Angela Merkel", "D) Indira Gandhi"},
+        {"A) Photosynthesis", "B) Respiration"," C) Fermentation"," D) Transpiration"},
+        {"A) William Shakespeare"," B) George Bernard Shaw"," C) Tennessee Williams"," D) Oscar Wilde"},
+        {"A) Yen"," B) Yuan","C) Dollar"," D) Euro"},
+        {"A) Hg"," B) He"," C) H", "D) Ho"},
+        {"A) Neil Armstrong"," B) Buzz Aldrin", "C) Yuri Gagarin", "D) Alan Shepard"}
+        };
+
+    public static String correctAnswers[] ={
+            "A) Fe",
+            "B) Harper Lee",
+            "D) Pacific Ocean",
+            "C) Paris",
+            "A) Alexander Fleming",
+            "A) 100°C",
+            "B) Salvador Dalí",
+            "D) NaCl",
+            "A) Venus",
+            "A) Margaret Thatcher",
+            "A) Photosynthesis",
+            "A) William Shakespeare",
+            "A) Yen",
+            "C) H",
+            "A) Neil Armstrong"
+    };
+}
